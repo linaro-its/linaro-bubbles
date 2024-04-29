8516703982c5dc541d4edb8360bcea231ff216b4
Philip Colmer <philip.colmer@linaro.org>
2024-04-29 13:48:50 +0100

    Add functioning code and example

--- linaro-bubbles.go
+++ linaro-bubbles.go
@@ -1,1 +1,58 @@
-package linarobubbles
+package main
+
+import (
+	"fmt"
+	"linaro-bubbles/picker"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type Model struct {
+	linpick picker.Model
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd = nil
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		}
+	}
+
+	m.linpick, cmd = m.linpick.Update(msg)
+
+	return m, cmd
+}
+
+func (m Model) View() string {
+	return m.linpick.View()
+}
+
+func main() {
+	items := []picker.Item{
+		{Key: "Prasanth", Value: "1"},
+		{Key: "Delara", Value: "2"},
+		{Key: "Louis", Value: "3"},
+		{Key: "Emmanuel", Value: "4"},
+	}
+	input := Model{linpick: picker.New(items)}
+	p := tea.NewProgram(input)
+	m, err := p.Run()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	if m, ok := m.(Model); ok {
+		cursor := m.linpick.Cursor()
+		key := m.linpick.Items()[cursor].Key
+		value := m.linpick.Items()[cursor].Value
+		fmt.Println(cursor, key, value)
+	}
+}
--- picker/picker.go
+++ picker/picker.go
@@ -2,15 +2,58 @@ package picker
 
 import tea "github.com/charmbracelet/bubbletea"
 
+type Item struct {
+	Key   string
+	Value string
+}
+
 type Model struct {
-	Prompt string
+	cursor int
+	items  []Item
 }
 
 func (m Model) View() string {
+	if len(m.items) == 0 {
+		return "no items"
+	}
 	// Render the currently selected item
-	return ""
+	return m.items[m.cursor].Key
 }
 
 func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "left":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "right":
+			if m.cursor < len(m.items)-1 {
+				m.cursor++
+			}
+		}
+	}
+
 	return m, nil
+}
+
+func (m Model) Cursor() int {
+	return m.cursor
+}
+
+func (m Model) Items() []Item {
+	return m.items
+}
+
+func (m *Model) SetItems(i []Item) {
+	m.items = i
+}
+
+func New(items []Item) Model {
+	m := Model{
+		cursor: 0,
+		items:  items,
+	}
+	return m
 }
